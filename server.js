e4749899f7f8c713680eb728e936c1dcbd5e9da8
Acoleman37 <aleccoleman123@yahoo.com>
2022-01-05 10:59:27 -0800

    built database calls

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 // Connection to Express.js server
-
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Connection to mysql Server
+const mysql = require('mysql2');
+
+// Connect to mysql database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Mcowart1!',
+        database: 'election'
+    },
+    console.log('Connect to the election database.')
+);
+
+// Return data from candidates
+db.query(`SELECT * FROM candidates`, (err,rows) => {
+    console.log(rows);
+});
+
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-// Create a GET test route 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
-});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
@@ -23,4 +34,4 @@ app.use((req, res) => {
 // Start the Express.js server on port 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
